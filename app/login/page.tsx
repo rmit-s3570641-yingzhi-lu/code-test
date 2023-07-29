f315404722e7f7d367172615e38630fa47449c67
Jodie Lu <jodie.lu@chemistwarehouse.com.au>
2023-07-29 22:26:00 +1000

    Move the login form out of the page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,81 +1,9 @@
-'use client';
+import LoginForm from "@/components/LoginForm"
 
-import { Alert } from "@/components/Alert";
-import { signIn } from 'next-auth/react'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useState } from 'react'
-
-export default function Login() {
-    const router = useRouter()
-    const searchParams = useSearchParams()
-    const callbackUrl = searchParams.get('callbackUrl') || '/'
-    const [username, setUserName] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
-
-   const onSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    try {
-      const res = await signIn('credentials', {
-        redirect: false,
-        username,
-        password,
-        callbackUrl
-      })
-      if (!res?.error) {
-        router.push(callbackUrl)
-      } else {
-        setError(res?.error ?? "Error signing in. Please try again.")
-      }
-    } catch (err: any) {}
-  }
-
+const loginForm = () => {
   return (
-    <div className="relative flex flex-col items-center p-20 min-h-screen overflow-hidden">
-      <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
-        <h1 className="text-3xl font-bold text-center text-gray-700">Login</h1>
-        <form onSubmit={onSubmit} className="mt-6">
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold text-gray-800"
-            >
-              Email
-            </label>
-            <input
-              value={username}
-              onChange={(e: any) => setUserName(e.target.value)}
-              type="email"
-              required
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="password"
-              className="block text-sm font-semibold text-gray-800"
-            >
-              Password
-            </label>
-            <input
-              value={password}
-              onChange={(e: any) => setPassword(e.target.value)}
-              type="password"
-              required
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-
-          {error && <Alert>{error}</Alert>}
-
-          <div className="mt-8">
-            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-              Login
-            </button>
-          </div>
-        </form>
-
-      </div>
-    </div>
-  );
+    <div><LoginForm/></div>
+  )
 }
+
+export default loginForm
---
+++ components/LoginForm.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import { Alert } from "@/components/Alert";
+import { signIn } from 'next-auth/react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useState } from 'react'
+
+export default function Login() {
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const callbackUrl = searchParams.get('callbackUrl') || '/'
+    const [username, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+
+    const isDisabled = () => !username || !password;
+
+    const onSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        try {
+            const res = await signIn('credentials', {
+                redirect: false,
+                username,
+                password,
+                callbackUrl
+            })
+            if (!res?.error) {
+                router.push(callbackUrl)
+            } else {
+                setError(res?.error ?? "Error signing in. Please try again.")
+            }
+        } catch (err: any) { }
+    }
+
+    return (
+        <div className="relative flex flex-col items-center p-20 min-h-screen overflow-hidden">
+            <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
+                <h1 className="text-3xl font-bold text-center text-gray-700">Login</h1>
+                <form onSubmit={onSubmit} className="mt-6">
+                    <div className="mb-4">
+                        <label
+                            htmlFor="username"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Username
+                        </label>
+                        <input
+                            value={username}
+                            onChange={(e: any) => setUserName(e.target.value)}
+                            type="email"
+                            required
+                            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            htmlFor="password"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Password
+                        </label>
+                        <input
+                            value={password}
+                            onChange={(e: any) => setPassword(e.target.value)}
+                            type="password"
+                            required
+                            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div>
+
+                    {error && <Alert>{error}</Alert>}
+
+                    <div className="mt-8">
+                        <button
+                            disabled={isDisabled()}
+                            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600 disabled:opacity-50">
+                            Login
+                        </button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    );
+}
